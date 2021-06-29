4705b3e49d210c253d1d5275ffe0def13da1438e
Yuriy Prosvirnin <prosvirniny@gmail.com>
2021-06-29 22:40:27 +0300

    + balls

--- Basketball AR/ViewController.swift
+++ Basketball AR/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    let configuration = ARWorldTrackingConfiguration()
+    var isBasketSet = false {
+        didSet {
+            //Setting up configuration detection off
+            configuration.planeDetection = []
+
+            // Run the view's session
+            sceneView.session.run(configuration, options: .removeExistingAnchors)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,9 +38,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-        
+
         //Setting up configuration to auto detect vertical and horizontal planes
         configuration.planeDetection = [.horizontal, .vertical]
 
@@ -54,7 +63,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return basketNode
     }
     
+    private func getBallNode() -> SCNNode {
+        let ball = SCNSphere(radius: 0.125)
+        ball.firstMaterial?.diffuse.contents = UIImage(named: "BasketballColor")
+        
+        let ballNode = SCNNode(geometry: ball)
+        if let frame = sceneView.session.currentFrame {
+            ballNode.simdTransform = frame.camera.transform
+        }
+        
+        return SCNNode(geometry: ball)
+    }
     
+    private func getPlaneNode(for anchor: ARPlaneAnchor) -> SCNNode {
+        let extent = anchor.extent
+        let plane = SCNPlane(width: CGFloat(extent.x), height: CGFloat(extent.z))
+        plane.firstMaterial?.diffuse.contents = UIColor.green
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.opacity = 0.3
+        planeNode.eulerAngles.x -= .pi/2
+        
+        return planeNode
+    }
+    
+    private func updatePlaneNode(_ node: SCNNode, for anchor: ARPlaneAnchor) {
+        guard let planeNode = node.childNodes.first, let plane = planeNode.geometry as? SCNPlane else {
+            return
+        }
+        
+        planeNode.simdPosition = anchor.center
+        plane.width = CGFloat(anchor.extent.x)
+        plane.height = CGFloat(anchor.extent.z)
+    }
 
     // MARK: - ARSCNViewDelegate
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
@@ -62,8 +103,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return
         }
         
-        node.addChildNode(getBasketNode())
+        node.addChildNode(getPlaneNode(for: planeAnchor))
+        
     }
     
-
+    func renderer(_ renderer: SCNSceneRenderer, willUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else {
+            return
+        }
+        
+        updatePlaneNode(node, for: planeAnchor)
+    }
+    
+    // MARK: - Actions
+    @IBAction func userTap(_ sender: UITapGestureRecognizer) {
+        
+        if isBasketSet {
+            
+            sceneView.scene.rootNode.addChildNode(getBallNode())
+            
+        }
+        else {
+            let location = sender.location(in: sceneView)
+            
+            guard let hitResult = sceneView.hitTest(location, types: .existingPlaneUsingExtent).first else {
+                return
+            }
+            
+            guard let anchor = hitResult.anchor as? ARPlaneAnchor, anchor.alignment == .vertical else {
+                return
+            }
+            
+            let basketNode = getBasketNode()
+            basketNode.simdTransform = hitResult.worldTransform
+            basketNode.eulerAngles.x -= .pi/2
+            
+            sceneView.scene.rootNode.addChildNode(basketNode)
+            
+            isBasketSet = true
+        }
+    }
+    
 }
